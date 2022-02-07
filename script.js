a0f667199617406bc1404c6cd72f489b2ff7d229
Nataliia <d_tasha@ukr.net>
2022-02-07 19:33:48 +0200

    added function findShort

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 "use strict";
 
 //1
-// const sumTo = (n) => (n === 1 ? n : n + sumTo(n - 1));
-// console.log(sumTo(1));
-// console.log(sumTo(2));
-// console.log(sumTo(3));
-// console.log(sumTo(4));
+const sumTo = (n) => (n === 1 ? n : n + sumTo(n - 1));
+console.log(sumTo(1));
+console.log(sumTo(2));
+console.log(sumTo(3));
+console.log(sumTo(4));
 
 //2
-function findShort(str) {
-  let array = str.split(" ");
-  array.sort(function (a, b) {
-    return a.length - b.length;
-  });
-  return array[0];
-}
-const sentance = "Lorem ipsum dolor sit amet";
+// function findShort(str) {
+//   let array = str.split(" ");
+//   array.sort(function (a, b) {
+//     return a.length - b.length;
+//   });
+//   return array[0];
+// }
+// const sentance = "Lorem ipsum dolor sit amet";
 
-console.log(findShort(sentance));
-console.log(findShort("Hello world"));
-console.log(findShort("Hi"));
-console.log(findShort("She is David's sister"));
+// console.log(findShort(sentance));
+// console.log(findShort("Hello world"));
+// console.log(findShort("Hi"));
+// console.log(findShort("She is David's sister"));
